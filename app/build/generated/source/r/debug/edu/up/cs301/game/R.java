ee2b8ef00d1b951ab13a1e1266f0a9e9562affd8
petersoe19 <emilyrp15@gmail.com>
2016-11-14 17:23:09 -0800

    buttons changed a little

--- app/build/generated/source/r/debug/edu/up/cs301/game/R.java
+++ app/build/generated/source/r/debug/edu/up/cs301/game/R.java
@@ -187,11 +187,10 @@ public final class R {
         public static final int face1=0x7f030000;
         public static final int faceone=0x7f030001;
         public static final int gamesquare=0x7f030002;
-        public static final int ic_launcher=0x7f030003;
-        public static final int playeronepiece=0x7f030004;
-        public static final int playerthreepiece=0x7f030005;
-        public static final int playertwopiece=0x7f030006;
-        public static final int playerzeropiece=0x7f030007;
+        public static final int playeronepiece=0x7f030003;
+        public static final int playerthreepiece=0x7f030004;
+        public static final int playertwopiece=0x7f030005;
+        public static final int playerzeropiece=0x7f030006;
     }
     public static final class string {
         public static final int add_player=0x7f080000;
