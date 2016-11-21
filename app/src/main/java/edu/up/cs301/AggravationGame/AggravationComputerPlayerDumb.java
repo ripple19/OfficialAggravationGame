6060f7694118f35fdfc361501e0415cf443ec164
OwenPriceSkelly <priceo19@up.edu>
2016-11-21 01:43:39 -0800

    Local Game finished, if it works - Owen

--- app/src/main/java/edu/up/cs301/AggravationGame/AggravationComputerPlayerDumb.java
+++ app/src/main/java/edu/up/cs301/AggravationGame/AggravationComputerPlayerDumb.java
@@ -10,8 +10,7 @@ import edu.up.cs301.game.infoMsg.GameInfo;
 /**
  * An dumb AI for Aggravation
  *
- * @author Andrew M. Nuxoll
- *          modified by Emily Peterson & Andrew Ripple & Owen Price
+ * @author Owen Price, Emily Peterson, Andrew Ripple
  * @version Nov 2016
  */
 public class AggravationComputerPlayerDumb extends GameComputerPlayer {
@@ -26,6 +25,8 @@ public class AggravationComputerPlayerDumb extends GameComputerPlayer {
 
     /**
      * callback method--game's state has changed
+     * dumbAI: start a piece -> move the piece closest to the start -> move the piece blocking that piece
+     * does not use shortcuts at all or aggravate intentionally, but should not miss a possible move otherwise
      *
      * @param info
      *        the information (presumably containing the game's state)
@@ -36,46 +37,48 @@ public class AggravationComputerPlayerDumb extends GameComputerPlayer {
        if (info instanceof AggravationState) {
            gameStateInfo = (AggravationState) info;
 
-        /*all the code is only if it's the correct turn*/
-           if (gameStateInfo.getTurn() == this.playerNum) {
+           if (gameStateInfo.getTurn() == playerNum) {
                Log.i("my", "turn!");
-            /*getRoll returns whether or there is a roll to be made - either the start of a turn or
-            * after rolling a 6 and making a complete move*/
                sleep(3000);
+               //getRoll returns whether or there is a roll to be made - either the start of a turn or
+               //after rolling a 6 and making a valid move
                if (gameStateInfo.getRoll()) {
                    AggravationRollAction rollAct = new AggravationRollAction(this);
                    game.sendAction(rollAct);
-                   return;
+               }
 
-               } else {//don't have to roll, so move a piece
-                   sleep(3000);
+
+               else {//don't have to roll, so move a piece
                    int value = gameStateInfo.getDieValue();
-                   int tempStart[] = gameStateInfo.getStartArray(playerNum);
-                   int tempGameBoard[] = gameStateInfo.getGameBoard();
+                   int startCopy[] = gameStateInfo.getStartArray(playerNum);
+                   int boardCopy[] = gameStateInfo.getGameBoard();
+                   int startIdx=playerNum*14;
+
+
                    if (value == 6 || value == 1) {
                     /*try to start whenever possible, so look through start array for a piece to move
                     and check the starting space to see if empty or an opponents piece to aggravate*/
                        for (int j = 0; j < 4; j++) {
                         /*playerNum *14 is where player starts based on index*/
-                           if (tempStart[j] == playerNum && tempGameBoard[playerNum * 14] != playerNum) {
-                            /*Send local game the move*/
+                           if (startCopy[j] == playerNum && boardCopy[startIdx] != playerNum) {
+                             /*Send local game the starting move*/
                                AggravationMovePieceAction startPiece =
-                                       new AggravationMovePieceAction(this, "Start", j, playerNum * 14);
+                                       new AggravationMovePieceAction(this, "Start", j, startIdx);
                                game.sendAction(startPiece);
                                return;
                            }
                        }
                    }
-                   //check to see if there is even a piece on the board the player can try to move
-                   int toMoveFrom = -1;
-                   for (int i = playerNum * 14; i < playerNum * 14 + 56; i++) {
+
+                   //check to see if there even is a piece on the board the CPU can look to move
+                   int toMoveFrom = -1;//default value, no -1 idx to confuse with
+                   for (int i = startIdx; i < startIdx + 56; i++) {
                        int j = i;
                        if (j > 56) {//so j always refers to a real index on the board
                            j = j - 56;
                        }
-
                     /*found a piece to (try to) move, so set toMoveFrom to that spot*/
-                       if (tempGameBoard[j] == playerNum) {
+                       if (boardCopy[j] == playerNum) {
                            toMoveFrom = j;
                            break;
                        }
@@ -85,15 +88,18 @@ public class AggravationComputerPlayerDumb extends GameComputerPlayer {
                    //if toMoveFrom!=-1 that means the previous loop found a space with playerNum as a value and is trying to move it
                    if (toMoveFrom != -1) {
                        for (int i = 1; i <= value; i++) {
-                           if (tempGameBoard[toMoveFrom + i] == playerNum) {
-                               toMoveFrom = +i;
-                               i = 1;
+                           if (boardCopy[toMoveFrom + i] == playerNum) {
+                               toMoveFrom = +i;//move from the idx of the piece blocking you, instead
+                               i = 1;//reset the loop
                            }
                        }
                        int toMoveTo = toMoveFrom + value;
-                       AggravationMovePieceAction movePieceGetOutTheWay = new AggravationMovePieceAction(this, "Board", toMoveFrom, toMoveTo);
+                       AggravationMovePieceAction movePieceGetOutTheWay;
+                       movePieceGetOutTheWay = new AggravationMovePieceAction(this, "Board", toMoveFrom, toMoveTo);
                        game.sendAction(movePieceGetOutTheWay);
-                   } else {//no pieces to move, so send a skip turn
+                   }
+
+                   else {//no pieces to move, so send a skip turn
                        AggravationMovePieceAction ff = new AggravationMovePieceAction(this, "Skip", 0, 0);
                        game.sendAction(ff);
                    }
--- app/src/main/java/edu/up/cs301/AggravationGame/AggravationLocalGame.java
+++ app/src/main/java/edu/up/cs301/AggravationGame/AggravationLocalGame.java
@@ -11,7 +11,7 @@ import java.util.Random;
 /**
  * class AggravationLocalGame controls the play of the game
  *
- * @author Emily Peterson, Andrew Ripple & Owen Price
+ * @author Owen Price, Emily Peterson, Andrew Ripple
  *
  * @version November 2016
  */
@@ -20,6 +20,7 @@ public class AggravationLocalGame extends LocalGame {
     /**
      * This ctor creates a new game state
      */
+
     private AggravationState officialGameState;
     private AggravationState copyGameState;
     private int actualRoll;
@@ -40,21 +41,22 @@ public class AggravationLocalGame extends LocalGame {
 
     /**
      * This method is called when a new action arrives from a player
-     *
+     * if it ever works add a bunch of shit here to explain
      * @return true if the action was taken or false if the action was invalid/illegal.
      */
     @Override
-    protected boolean makeMove(GameAction action) {
+    protected synchronized boolean makeMove(GameAction action) {
+            //^^^^^^^^^^^^//ask Vegdahl about this in particular - Owen
+
         int playerNum=getPlayerIdx(action.getPlayer());
         int boardCopy[] = officialGameState.getGameBoard();
         int startCopy[]= officialGameState.getStartArray(playerNum);
         int homeCopy[] = officialGameState.getHomeArray(playerNum);
 
 
-        if(action instanceof AggravationRollAction)
-        {
+        if(action instanceof AggravationRollAction) {
             Random dieValue = new Random();//dieValue outside of the conditionals
-            int value = dieValue.nextInt(6-1+1) + 1;
+            int value = dieValue.nextInt(6) + 1; //"As a mathematician," I took out the +1-1 b/c why would it be there? - Owen
             actualRoll = value;
             Log.i("dieVal is ", Integer.toString(value));
             officialGameState.setDieValue(value);
@@ -62,8 +64,9 @@ public class AggravationLocalGame extends LocalGame {
             officialGameState.setRoll(false);
             System.out.println("Roll = " + value);
             Log.i("set value", Integer.toString(value));
-            sendAllUpdatedState();
-            return true;
+            //took these two lines out because I think they were causing the thread problems - Owen
+            //sendAllUpdatedState();
+            //return true;
         }
         else if(action instanceof AggravationMovePieceAction)
         {
@@ -76,8 +79,8 @@ public class AggravationLocalGame extends LocalGame {
                 endOfTheLine = 54;
             }
             if (type.equalsIgnoreCase("Start")) {
-                newIdx=playerNum*14;//safety net-if a starting move is passed with an index that isn't the start space
-                if(boardCopy[newIdx]!= -1){ //the new index is occupied
+                newIdx=playerNum*14;//safety net-if a starting move is somehow passed with an index that isn't the start space
+                if(boardCopy[newIdx]!= -1){//the new index is occupied
                     if (boardCopy[newIdx]==playerNum) {//the player occupies their own start spot
                         return false;//not a valid move, they have to move out of the way
                     }
@@ -102,6 +105,9 @@ public class AggravationLocalGame extends LocalGame {
                 boardCopy[newIdx]=playerNum;
                 officialGameState.setGameBoard(boardCopy);
             }
+            /* Call your move a "Home" move if you are moving TO the home array
+            * from the board or within the home array itself - Owen
+            */
             else if (type.equalsIgnoreCase("Home")){
                 boolean fromOuterSpace=false;//"outer space" being the board array
                 if(newIdx>3){//should only be needed for CPU players, since there aren't any buttons
@@ -133,6 +139,10 @@ public class AggravationLocalGame extends LocalGame {
                 officialGameState.setHomeArray(playerNum,homeCopy);
             }
 
+            /* Call your move a "Board" move if you are moving TO somewhere on the board, unless it's a start
+            * the shortcut spaces count as "Board" spaces, so unless you're trying to land on the actual center
+            * and are actually TAKING a shortcut / cutting something short, call it a "Board" move
+            - Owen */
             else if (type.equalsIgnoreCase("Board")) {
                 //return false if you would be "leapfrogging" one of your own
                 //Should never happen with CPU
@@ -161,43 +171,162 @@ public class AggravationLocalGame extends LocalGame {
                             break;
                         }
                     }
-
                 }
                 //out with the old, in with the new
                 boardCopy[oldIdx]=-1;
                 boardCopy[newIdx]=playerNum;
                 officialGameState.setGameBoard(boardCopy);
             }
-            else if (type.equalsIgnoreCase("Shortcut")){
-                boolean exactRoll = false;
-                //if (/*new idx is the center piece*/){
-                    //code to check if its an exact roll
-                    //exactRoll =true;
-                //}
-                //if(/*newIdx is a regular shortcut*/){
-                    //check if exactRoll
-                    //exactRoll=true;
-                //}
-                if(!exactRoll){
-                    return false;
+
+                /* the 4 shortcut spaces are 5, 19, 33, and 47
+                * if oldIdx = any of these, then enable shortcut moves?
+                * a valid shortcut move is based exclusively on the space
+                * you can loop all the way around if you want, which means it doesn't have to be
+                * based on the player at all valid newIdx's:
+                * i from 0 to the die value - there are as many valid new indexes as the die value
+                * newIdx == oldIdx + (14*i) + value - i
+                * 14*i is b/c you move a full quarter of the 56 piece board jumping shortcut spot to shortcut spot
+                * value -i is b/c each jump from cut to cut is 1 less you can move after
+                * newIdx == oldIdx+value + 13*i
+                * - Owen
+                */
+
+            /* Call your move a "shortcut" move if you are moving IN "shortcut space"
+             * "Am I taking a shortcut?" is the key question here, so any moves
+             * TO the center, or FROM a regular shortcut space
+             * are moves you should label "Shortcut."
+             * Any moves to just a regular shortcut space should be labeled Board
+             * if this sounds unintuitive and backwards I get it but I have my reasons - Owen
+             * */
+            else if (type.equalsIgnoreCase("Shortcut")) {
+
+                //canCutThere -"Can you take a shortcut there?" for validity of move,
+                // last shortcut is an idx for leapfrogging purposes
+                boolean canCutThere = false;
+                int lastShortcut=0;
+
+                /*the middle space stuff has more code than I'd like,
+                * the 3 line if-statement really hurts my aesthetic.
+                * basically, if you're trying to move to the middle spot, it makes sure that
+                * you land on a shortcut immediately before hitting the middle spot, so you see that it's
+                * landing on the center by an exact count, then makes sure neither that spot nor the center spot
+                * is the player's piece (leapfrog check), then it aggravates if someone's there - Owen */
+                if (newIdx==57) {
+                    //oneOff is the shortcut spot right before the middle reached by exact count
+                    int oneOff = oldIdx+actualRoll-1;
+
+                    if ((oneOff==5||oneOff==19||oneOff==33||oneOff==47)
+                            && (boardCopy[oneOff]!= playerNum)
+                            && (boardCopy[57]!=playerNum)){
+
+                        if(boardCopy[57]!=-1){//aggravation copypaste
+                            int otherPlayerNum = boardCopy[newIdx];
+                            int otherStart[] = officialGameState.getStartArray(otherPlayerNum);
+                            for (int i=0;i<4;i++){
+                                if (otherStart[i]==-1){
+                                    otherStart[i]=otherPlayerNum;
+                                    officialGameState.setStartArray(otherPlayerNum,otherStart);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                /*moving from any of the 4 possible shortcut spaces,
+                * check the given newIdx with every possible valid point you can move to
+                * you're allowed to loop around as many times as you want on the shortcut spaces, apparently,
+                * and if this works like it does in my head that should be totally fine - Owen*/
+                if (oldIdx==5||oldIdx==19||oldIdx==33||oldIdx==47){
+                    for(int i = 0; i<=actualRoll;i++){
+
+                        //valid is a valid new index
+                        //yes I did mean 13 not 14 check my work the i distributes - Owen
+                        int valid = oldIdx+actualRoll+(13*i);
+
+                        //keep it on the board
+                        //while loop since we want it to stay relative to the 56 square board
+                        while (valid>56) {
+                            valid=valid-56;
+                            //I feel like my algebra is questionable, but I don't see any reason this wouldn't work
+                            //valid = oldIdx+actualRoll+(13*i)
+                            //oldIdx+14*i = the index of the furthest shortcut reached before
+                            //leaving shortcut space for the main board
+                            //so lastShortcut = oldIdx + 14*i = valid - actualRoll + i
+                            //since valid is decremented I put in terms of valid - Owen
+                            lastShortcut = valid-actualRoll+i;
+                        }
+
+                        //special leapfrog check, breaks the loop before validating
+                        //a newIdx if you have another piece on that shortcut space
+                        //don't need to keep it relative to the 56, since it would break before
+                        //i got greater than 3, if it was going to break
+                        if(i!=0 && boardCopy[oldIdx+14*i]==playerNum)
+                            break;
+
+
+                        //if the newIdx matches with a valid spot, you can take the
+                        // short cut you asked for and break the loop early
+                        if(valid == newIdx) {
+                            canCutThere = true;
+                            break;
+                        }
+                    }
+                }
+                else return false;//don't make a shortcut move if you aren't moving from a shortcut space
+
+                if(!canCutThere) return false;//if you don't match with any of the possible valid moves
+                else {//if you can, check for leapfrog and aggravations from the shortcut you left from
+                    //ideally, since every move has such similar leapfrog/aggravation code, I would be able to
+                    //move this bit outside of the "type" cases. Not important for functionality yet though - Owen
+
+                    for (int i=lastShortcut;i<=newIdx;i++){//leapfrog check
+                        if (boardCopy[i] == playerNum)
+                            return false;
+                    }
+
+                    if (boardCopy[newIdx]!=-1){//aggravation copypasta
+                        int otherPlayerNum = boardCopy[newIdx];
+                        int otherStart[] = officialGameState.getStartArray(otherPlayerNum);
+                        for (int i=0;i<4;i++){
+                            if (otherStart[i]==-1){
+                                otherStart[i]=otherPlayerNum;
+                                officialGameState.setStartArray(otherPlayerNum,otherStart);
+                                break;
+                            }
+                        }
+                    }
+                    //out with the old, in with the new
+                    boardCopy[oldIdx]=-1;
+                    boardCopy[newIdx]=playerNum;
+                    officialGameState.setGameBoard(boardCopy);
                 }
             }
+
+            /*Label a move "skip" if you want to make a move,
+            * but are afraid of commitment */
             else if (type.equalsIgnoreCase("Skip"))
             {
-
+                //99% sure we can take this out safely since we don't want it to do anything - Owen
             }
-
-            if(actualRoll == 6) //if the player rolled a 6 CHANGE THIS
-            {
-                System.out.println("Roll was a 6.");
-                officialGameState.setRoll(true);
-                sendAllUpdatedState();
-                return true;
-            }
+            //after any actual move is made, someone has to roll
+            officialGameState.setRoll(true);
         }
-        Log.i("changed turn to ",Integer.toString(playerNum +1));
-        officialGameState.setTurn(playerNum + 1); //CHANGE THIS
-        officialGameState.setRoll(true);
+
+        //if the player *didn't* roll a 6 - I moved this outside
+        // to the same scope as the other "return true" statement and cleaned it up a bit
+        // there was also an extra 2 lines at the end of "RollAction" - I think
+        // part of our problem was that we called "sendAllUpdatedState" at least twice
+        // for any given move. I've also made sure that every other return statement is a return false,
+        // so makeMove can't return true prematurely. Between this and the synchronization thing I think I probably fixed
+        // the roll problem. Bear that in mind when the rest of the code I added inevitably fucks everything up - Owen
+        if(actualRoll != 6) {
+            Log.i("changed turn to ",Integer.toString(playerNum +1));
+            officialGameState.setTurn(playerNum + 1);
+        }
+        else System.out.println("Roll was a 6.");
+
+        //last 2 lines aren't anywhere else anymore
         sendAllUpdatedState();
         return true;
     }//makeMove
@@ -221,10 +350,33 @@ public class AggravationLocalGame extends LocalGame {
      */
     @Override
     protected String checkIfGameOver() {
+        int[] counts = new int[4];
+        int[][] homeCopy = officialGameState.getHomeArray();
+
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                if (homeCopy[i][j]==i){
+                    counts[i]++;
+                }
+            }
+            if (counts[i]==4){
+                return ("Player "+i+" Wins!");
+            }
+            else {
+                counts[i] = 0;//reset the count
+            }
+        }
+        return null;
+
+        /* I left the other method here in case you wanted it back, I wasn't trying to be
+        passive aggressive or anything it was just an easy fix - Owen
+
+
         int countZeroPiece = 0;
         int countOnePiece = 0;
         int countTwoPiece = 0;
         int countThreePiece = 0;
+
         for(int i = 0;i < 4; i++)
         {
             int[][] tempGameOver = officialGameState.getHomeArray();
@@ -269,7 +421,6 @@ public class AggravationLocalGame extends LocalGame {
         if(countThreePiece==4){
             return "Player 3 Wins!";
         }
-        return null;
+        return null;*/
     }
-
 }// class AggravationLocalGame
